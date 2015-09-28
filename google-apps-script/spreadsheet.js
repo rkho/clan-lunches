2e7f091cc040b9a2a57c4a66df7e6c53fbab9e30
David Ernst <dsernst@users.noreply.github.com>
2015-09-27 22:37:00 -0700

    Create spreadsheet.js

---
+++ google-apps-script/spreadsheet.js
@@ -0,0 +1,26 @@
+var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+
+var usersSheet = spreadsheet.getSheetByName('Users')
+var responseSheet = spreadsheet.getSheetByName('wk2 responses')
+
+var getData = _.memoize(function () {
+  return usersSheet.getDataRange().getValues()
+})
+
+var getUsers = _.memoize(function() {
+  return arraysToObjects(getData())
+})
+
+var getResponses = _.memoize(function () {
+  return arraysToObjects(responseSheet.getDataRange().getValues())
+})
+
+function getColHeaders() {
+  return getData()[0]
+}
+
+var nameCol = "What's your name?"
+var emailCol = "What's your email?"
+var addressCol = "What's your office address? (Include city)"
+var maxDistanceCol = 'How far are you willing to travel?'
+var daysCol = 'What days are you available next week?'
